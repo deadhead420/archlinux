cc91828bf85fb93bc3b5a7c5090e9a0b96f6ec4b
deadhead420 <deadhead3492@gmail.com>
2015-06-19 04:01:24 -0400

    update

--- wiki/wiki.sh
+++ wiki/wiki.sh
@@ -4,7 +4,7 @@ var=$(echo "$@" | sed 's/ /_/g')
 
 attempt_search=$(lynx -dump wiki.archlinux.org/index.php/$var | grep "There is currently no text in this page" | cut -c57-) #Make sure page exists
 	if [ "$attempt_search" = "search for this" ]; then
-		lynx "https://wiki.archlinux.org/index.php?title=Special%3ASearch&search=$var&fulltext=Search"
+		lynx "https://wiki.archlinux.org/index.php?title=Special%3ASearch&search=$var&fulltext=Search" #Search the arch wiki
 	else
 		lynx "https://wiki.archlinux.org/index.php/$var" #Connect directly to wiki page
 fi
