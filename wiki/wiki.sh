c22d77813615ccc742755094c14154fcf8d635a0
deadhead420 <deadhead3492@gmail.com>
2015-06-19 03:07:13 -0400

    added support

--- wiki/wiki.sh
+++ wiki/wiki.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 ## Attempt to connect to search
-attempt_search=$(lynx -dump wiki.archlinux.org/index.php/$1 | grep "There is currently no text in this page") #Make sure page exists
+
+var=$(echo "$@" | sed 's/ /_/g')
+
+attempt_search=$(lynx -dump wiki.archlinux.org/index.php/$var | grep "There is currently no text in this page") #Make sure page exists
 	if [ "$attempt_search" = "search for this" ]; then
-		lynx https://wiki.archlinux.org/index.php?title=Special%3ASearch&profile=default&search=$1&fulltext=Search #Connect to search results
+		lynx https://wiki.archlinux.org/index.php?title=Special%3ASearch&profile=default&search=$var&fulltext=Search #Connect to search results
 	else
-		lynx https://wiki.archlinux.org/index.php/$1 #Connect directly to wiki page
+		lynx https://wiki.archlinux.org/index.php/$var #Connect directly to wiki page
 fi
