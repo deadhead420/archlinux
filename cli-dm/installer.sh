5975c7227c5a5e3b0e2a6c4c10045c80efb9e6d7
deadhead420 <deadhead3492@gmail.com>
2015-09-16 02:35:35 -0400

    Command Line Display Manager Installer

--- cli-dm/installer.sh
+++ cli-dm/installer.sh
@@ -20,7 +20,7 @@ else
 	echo "*sudo not detected* it is recommended to use sudo for administration from a user account."
 	echo
 	echo "To install cli-dm please provide the root password"
-	su -c 'wget -O - https://raw.githubusercontent.com/deadhead420/archlinux/master/wiki/installer.sh | sh'
+	su -c 'wget -O - https://raw.githubusercontent.com/deadhead420/archlinux/master/cli-dm/installer.sh | sh'
 fi
 }
 root_account
